48e3f0b5e71e87d95ca7dd9ac9a303bebb2c6547
Anshu-kumar-github <anshu.kumaar712@gmail.com>
2026-06-28 23:58:40 +0530

    no changes made

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,20 +1,3 @@
-// import { Navigate } from "react-router-dom";
-// import { useAuth } from "../context/AuthContext";
-
-// const ProtectedRoute = ({ children }) => {
-
-//   const { token } = useAuth();
-
-//   if (!token) {
-//     return <Navigate to="/" />;
-//   }
-
-//   return children;
-
-// };
-
-// export default ProtectedRoute;
-
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
